5d00c494e0609e5faa2ff9b9fc1f74d1a6b996d1
theroyakash <business.theroyakash@outlook.com>
2020-04-10 15:16:50 +0530

    Neomorphic button added

--- Minimal Store/Minimal Store/Views/ItemRow.swift
+++ Minimal Store/Minimal Store/Views/ItemRow.swift
@@ -13,13 +13,19 @@ struct ItemRow: View {
     var item: MenuItem
     
     var body: some View {
-        VStack(alignment: .leading){
-            Image(item.mainImage)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            
-            Text(item.name).padding(.top)
-            Text("$\(item.price, specifier: "%.2f")").padding(.bottom)
+        
+        NavigationLink(destination: ItemDetail()){
+        
+            VStack(alignment: .leading){
+                Image(item.mainImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.top, 30)
+                
+                Text(item.name).padding(.top)
+                Text("$\(item.price, specifier: "%.2f")").padding(.bottom)
+            }
+                
         }
     }
 }
