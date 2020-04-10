5918c0702508fb883473d671c5279cf1d524a770
theroyakash <business.theroyakash@outlook.com>
2020-04-10 13:57:24 +0530

    file added

---
+++ Minimal Store/Minimal Store/Assets.xcassets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Minimal Store
+//
+//  Created by Roy Akash on 10/04/20.
+//  Copyright © 2020 theroyakash. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
