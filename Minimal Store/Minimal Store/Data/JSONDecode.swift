2d32000f8b54586b1f8acbf5b49610e671d7716f
theroyakash <business.theroyakash@outlook.com>
2020-04-10 14:54:29 +0530

    JSON Decorer Added

---
+++ Minimal Store/Minimal Store/Data/JSONDecode.swift
@@ -0,0 +1,30 @@
+//
+//  JSONDecode.swift
+//  Minimal Store
+//
+//  Created by Roy Akash on 10/04/20.
+//  Copyright © 2020 theroyakash. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension Bundle {
+    func decode<T: Decodable>(_ type: T.Type, from file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+
+        let decoder = JSONDecoder()
+
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+
+        return loaded
+    }
+}
